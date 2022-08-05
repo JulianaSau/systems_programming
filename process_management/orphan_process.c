4837704d0be036c5a9a3d7fde564d69ef0659f73
Juliana Sau <julienyange@gmail.com>
2022-08-05 06:51:21 +0300

    Create orphan_process.c

---
+++ process_management/orphan_process.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	pid_t child_pid = fork();
+	
+	if(child_pid > 0)
+	{
+		printf("In Parent Process\n");
+
+	}
+	else if(child_pid == 0)
+	{
+		printf("In Child Process \n");
+		//making child process sleep for some time
+		sleep(10);
+		printf("After sleep time);	
+	}
+	else if(child_pid < 0)
+	{
+		printf("Child Process creation failed\n");
+	}
+
+	return(0);
+}
