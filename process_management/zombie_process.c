9b2e4e10e7aab6cceea4cb391fa3fad843a9afed
Juliana Sau <julienyange@gmail.com>
2022-08-05 06:45:44 +0300

    Create zombie_process.c

---
+++ process_management/zombie_process.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	pid_t child_pid = fork();
+	
+	if(child_pid > 0)
+	{
+		printf("In Parent Process\n");
+		//making parent process sleep for some time
+		sleep(10);
+	}
+	else if(child_pid == 0)
+	{
+		printf("In Child Process \n");
+		exit(0);
+		
+	}
+	else if(pid < 0)
+	{
+		printf("Child Process creation failed\n");
+	}
+
+	return(0);
+}
